f90a59717245ed72fa70acc52ffe72c541929674
clonemode <davidika2001@gmail.com>
2021-10-28 07:34:25 +0200

    save
    saving progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if(GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        PlayerPrefs.DeleteAll();
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    ///Ressources
+    public List<Sprite> playerSprite;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrice;
+    public List<int> xpTable;
+
+    ///References
+    public Player Player;
+    // public weapon weapon...
+
+    //Logic
+    public int pesos;
+    public int experience;
+
+    //Save
+    /*
+     * Int preferedSkin
+     * Int pesos
+     * Int exp
+     * INT weaponlvl
+     * 
+     */
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        PlayerPrefs.SetString("SaveState", s);
+    }
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        //Change player skin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //Change the weapon level
+
+        Debug.Log("LoadState");
+    }
+}
